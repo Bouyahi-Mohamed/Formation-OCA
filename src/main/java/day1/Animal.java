3cbaa2c07076cc5b744e266f8c82adb7da30683f
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-12-06 10:56:22 +0100

    test le bloc de class

--- src/main/java/day1/Animal.java
+++ src/main/java/day1/Animal.java
@@ -1,8 +1,48 @@
 package day1;
 
 public class Animal {
-    // les block
- // les attributs d'instance
- //les attributs de class 
+    // Les attributs d'instance
+    String espece;
+    int age;
 
+    //Les attributs de classe
+    static int  MAX_AGE = 200;
+
+    //Les constructeurs
+    public Animal() {
+        System.out.println("Constructeur par défaut Animal");
+    }
+    public Animal(String espece, int age) {
+        System.out.println("Constructeur avec paramètres Animal");
+        this.espece = espece;
+        this.age = age;
+    }
+
+    // Les méthodes d'instance
+    public String getEspece() {
+        return espece;
+    }
+    public void setEspece(String espece) {
+        this.espece = espece;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Les méthodes de classe
+    public static int getMAX_AGE() {
+        return MAX_AGE;
+    }
+    public static void setMAX_AGE(int mAX_AGE) {
+        MAX_AGE = mAX_AGE;
+    }
+
+
+
+
+    // Les blocs d'initialisation de classe
+    // Les blocs d'initialisation d'instance
 }
---
+++ src/main/java/day1/TestAnimal.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class TestAnimal {
+
+    public static void main(String[] args) {
+        Animal a1 = new Animal();
+        Animal a2 = new Animal("Chat",2);
+
+    }
+
+}
