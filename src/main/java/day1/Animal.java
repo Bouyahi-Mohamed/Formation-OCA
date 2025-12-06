510009414b1a50a05260da98a119a3c31c12acec
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-12-06 10:31:24 +0100

    Day one 06-12-2025

---
+++ src/main/java/day1/Animal.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class Animal {
+    // les block
+ // les attributs d'instance
+ //les attributs de class 
+
+}
---
+++ src/main/java/day1/main.java
@@ -0,0 +1,28 @@
+package day1;
+
+public class main   {
+public static  void main(String[] args) {
+    // MAX VALUE
+        // Byte
+    System.out.println(Byte.MAX_VALUE);
+    System.out.println(Byte.MIN_VALUE);
+        //short
+    System.out.println(Short.MAX_VALUE);
+    System.out.println(Short.MIN_VALUE);
+    //Interger
+    System.out.println(Integer.MAX_VALUE);
+    System.out.println(Integer.MIN_VALUE);
+    // Long
+    System.out.println(Long.MAX_VALUE);
+    System.out.println(Long.MIN_VALUE);
+        //Flot
+    System.out.println(Float.MAX_VALUE);
+    System.out.println(Float.MIN_VALUE);
+        //double
+    System.out.println(Double.MAX_VALUE);
+    System.out.println(Double.MIN_VALUE);
+
+
+
+}
+}
